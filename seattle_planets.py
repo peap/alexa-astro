5bce6507572baf606b7da8a28a3c7daa6e3d66ec
Eric Petersen <eric@huskers.unl.edu>
2016-02-15 22:52:19 -0800

    Add example code for finding visible planets from Seattle
    Add venv to .gitignore
    Also add .editorconfig, pip requirements file, and flake8 config

---
+++ seattle_planets.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import ephem
+
+
+INTERESTING_PLANETS = [
+    ephem.Mercury,
+    ephem.Venus,
+    ephem.Mars,
+    ephem.Jupiter,
+    ephem.Saturn,
+    ephem.Uranus,
+    ephem.Neptune,
+]
+
+MIN_ALT = 5.0 * ephem.degree
+
+
+def get_observer(city):
+    observer = ephem.Observer()
+    # TODO: lookup coordinates... it's just Seattle, for now
+    observer.lat = 47.606
+    observer.lon = -122.332
+    return observer
+
+
+def get_visible_planets(observer):
+    visible = []
+    for Planet in INTERESTING_PLANETS:
+        planet = Planet()
+        planet.compute(observer)
+        if planet.alt >= MIN_ALT:
+            visible.append(planet)
+    return visible
+
+
+if __name__ == '__main__':
+    seattle_observer = get_observer('Seattle, WA')
+    for planet in get_visible_planets(seattle_observer):
+        altitude = round(planet.alt / ephem.degree)
+        azimuth = round(planet.az / ephem.degree)
+        print(
+            '{0.name}: azimuth {1} degrees, altitude {2} degrees'
+            .format(planet, azimuth, altitude)
+        )
